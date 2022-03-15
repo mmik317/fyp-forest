13c833543da23307a1785c95b9d7bf59ec37cd92
Ziayan Javed <ziayanj@gmail.com>
2022-03-15 20:58:54 +0500

    Implement returning results' file path

--- amplify/backend/function/inferenceFunc/src/index.py
+++ amplify/backend/function/inferenceFunc/src/index.py
@@ -13,3 +13,18 @@ def handler(event, context):
       },
       'body': json.dumps('Hello from your new Amplify Python lambda!')
   }
+
+def resultFile(region, starting_year, ending_year):
+    region = region.lower().replace(" ", "_")
+
+    if starting_year == ending_year:
+        year = starting_year
+        path = '{}_{}.png'.format(region, year)
+        return '/mnt/c/Users/Ziaya/Desktop/Datasets Forest/Digitized_Forest_Maps_2014_2020_png/' + path
+    else:
+        path = '{}_{}_{}_2.png'.format(region, starting_year, ending_year)
+        return '/mnt/c/Users/Ziaya/Desktop/Datasets Forest/comparison-2/' + path
+
+        
+print(resultFile("Haripur", 2015, 2015))
+print(resultFile("Hariput", 2015, 2019))
